6b7056dc114f41a35ebef19af61cda6f6d11452f
onjmin <88383494+onjmin@users.noreply.github.com>
2021-09-23 19:29:03 +0900

    Update killKote.js

--- killKote.js
+++ killKote.js
@@ -1,53 +1,60 @@
-const defaultName = '名無しさん';
-const latest = {
-    toString: () => latest.n,
-    update: () => (latest.n = local_resnum)
-};
-latest.update();
-socket.on('u', n => {
-    $.ajax({
-        type: 'GET',
-        url: `/ajax/get_res.v7.cgi/${bbs}/${bbskey}/`,
-        data: `l=${latest}&s=${server_resnum}`,
-        cache: true
-    }).done(r => {
-        const late = [],
-              ar = r.split('<dt').flatMap(v => {
-                  const id = v.match(/uid="(.+?)"/)?.[1],
-                        n = v.match(/res="(.+?)"/)?.[1],
-                        name = v.match(/<b>(.+?)<\/b>/)?.[1],
-                        is1 = /red>主<\/font>/.test(v);
-                  if(!id || is1 || late.includes(id) || name === defaultName) return [];
-                  late.push(id);
-                  return `>>${n}`;
-              });
-        if(ar.length) post([...ar, '!aku'].join('\n'));
+(async () => {
+    const akuNinja = true; // 忍法帖も含める
+    const defaultName = await new Promise(resolve => {
+        $.ajax({
+            type: 'GET',
+            url: `/test/read.cgi/${bbs}/${bbskey}/1`
+        }).done(r => resolve(r.match(/name><b>(.+?)<\/b>/)?.[1]))
     });
-    latest.update();
-});
-const post = str => $.ajax({
-    type: 'POST',
-    url: '/test/bbs.cgi',
-    cache: false,
-    data: {
-        FROM: '',
-        mail: '',
-        sage: 1,
-        ninja: 0,
-        rating: 0,
-        no_nusi: 0,
-        MESSAGE: str,
-        bbs: bbs,
-        key: bbskey,
-        submit: '書',
-        mode: 'ajax',
-        zitumeiMode: 0,
-        timelineMode: 0,
-        parent_pid: '',
-        twfunc: 0,
-        twid: 0,
-        twsync: 0,
-        oekakiMode: 0,
-        oekakiData: '',
-    }
-});
+    socket.on('u', n => {
+        $.ajax({
+            type: 'GET',
+            url: `/ajax/get_res.v7.cgi/${bbs}/${bbskey}/`,
+            data: `l=${n - 1}&s=${n}`,
+            cache: true
+        }).done(r => {
+            const late = [],
+                  ar = r.split('<dt').flatMap(v => {
+                      const id = v.match(/uid="(.+?)"/)?.[1],
+                            n = v.match(/res="(.+?)"/)?.[1],
+                            is1 = /red>主<\/font>/.test(v);
+                      if(!id || is1 || late.includes(id)) return [];
+                      const name = [
+                          /▲.+$/,
+                          /▼.+$/,
+                          akuNinja ? null : '■'
+                      ].filter(v => v).reduce((p, x) => p.replace(x, ''), v.match(/<b>(.+?)<\/b>/)?.[1]);
+                      if(defaultName === name || !name) return [];
+                      late.push(id);
+                      return `>>${n}`;
+                  });
+            if(ar.length) post([...ar, '!aku'].join('\n'));
+        });
+    });
+    const post = str => $.ajax({
+        type: 'POST',
+        url: '/test/bbs.cgi',
+        cache: false,
+        data: {
+            FROM: '',
+            mail: '',
+            sage: 1,
+            ninja: 0,
+            rating: 0,
+            no_nusi: 0,
+            MESSAGE: str,
+            bbs: bbs,
+            key: bbskey,
+            submit: '書',
+            mode: 'ajax',
+            zitumeiMode: 0,
+            timelineMode: 0,
+            parent_pid: '',
+            twfunc: 0,
+            twid: 0,
+            twsync: 0,
+            oekakiMode: 0,
+            oekakiData: '',
+        }
+    });
+})();
